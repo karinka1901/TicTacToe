d9a0dab075e3e0304d92e010147c3a9e80de4cec
karinka1901 <karindomagalska@gmail.com>
2024-03-21 20:42:30 +0200

    fixed some errors

--- tic-tac-toe/Database.h
+++ tic-tac-toe/Database.h
@@ -2,6 +2,8 @@
 #include "json.hpp"
 #include <map>
 #include <tuple>
+#include <fstream>
+#include <iomanip>
 
 using json = nlohmann::json;
 using namespace std;
@@ -10,10 +12,11 @@ class TicTacToe;
 
 class Database {
 public:
+
 	map<string, tuple<char, string>> player_data; // name, symbol, game mode
 
     void write_player_data(const char* player_name, TicTacToe* game);
-    map<string, tuple<char, string>> &get_player_data(); //for imgui
+    map<string, tuple<char, string>> &get_player_data();
     void read_player_data();
 	void saveToFile();
     void loadFromFile();
